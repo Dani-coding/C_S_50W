d4fa378321f5a8e92d33a96bc255ed4d38f164a8
Dani-coding <daniel.bedouret@gmail.com>
2023-07-11 00:19:25 +0200

    Send is working. Mailbox too, but not catch errors yet

--- Project3/mail/mail/static/mail/inbox.js
+++ Project3/mail/mail/static/mail/inbox.js
@@ -20,8 +20,15 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+  document.querySelector("#compose-submit").addEventListener("click", send_data)
 
-  if  (request.method == "POST"){
+
+function send_data(){
+    datos = JSON.stringify({
+      recipients: document.querySelector("#compose-recipients").value,
+      subject: document.querySelector("#compose-subject").value,
+      body: document.querySelector("#compose-body").value,
+    })
     fetch("/emails",{
       method: "POST",
       body: JSON.stringify({
@@ -32,25 +39,51 @@ function compose_email() {
     })
     .then(response => response.json())
     .then(result => {
-      if (response.status == 201){
-        alert(response.message);
+      if (result.status == 201){
         load_mailbox("sent")
       }
-      else if (response. status == 400){
-        alert(reponse.error)
-        document.querySelector("#compose-recipients").focus();
+      else if (result.status == 400){
+        document.querySelector("#compose-recipients").focus();    // --------- También falta gestionar errores !!!
       }
 
-    })
+    });
   }
 }
 
 function load_mailbox(mailbox) {
-  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  fetch("/emails/" + mailbox)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+    show_mails(emails);
+  })
+  .catch(error => console.log(error.message)) // ------------------- No funciona, no pilla el error
+  
+
+  //--------- reaccionar a eventos, por ejemplo, pinchar e ir a mail
+}
+
+function show_mails(emails){
+  const view = document.querySelector("#emails-view");
+  console.log("entra el show_mails y la variables vale: "+emails);
+  emails.forEach(mail => {
+    const entry = document.createElement("div");
+    entry.className="entry";
+    entry.className="border border-dark";
+    entry.innerHTML="From:" + mail.sender + " ; Subject: " + mail.subject + " ; In:" + mail.timestamp;
+    if ( !mail.read ){
+      entry.style.backgroundColor = "red";
+    }
+    else{
+      entry.style.backgroundColor = "white";
+
+    }
+    view.appendChild(entry);
+  });
 }
